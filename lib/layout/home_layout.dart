6a463f31bf9778a5eabd4c49974acf8443bf55b9
salemali21 <73366946+salemali21@users.noreply.github.com>
2023-03-03 22:03:24 +0200

    Initial commit

---
+++ lib/layout/home_layout.dart
@@ -0,0 +1,259 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todoapp/shared/components/components.dart';
+import 'package:todoapp/shared/cubit/cubit.dart';
+import 'package:todoapp/shared/cubit/states.dart';
+import 'package:intl/intl.dart';
+
+import '../shared/components/constants.dart';
+
+class HomeLayout extends StatelessWidget {
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+  var formKey = GlobalKey<FormState>();
+  var titleController = TextEditingController();
+  var timeController = TextEditingController();
+  var dateController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (BuildContext context) => AppCubit()..createDB(),
+      child: BlocConsumer<AppCubit, AppStates>(
+        listener: (BuildContext context, AppStates state) {
+          if (state is AppInsertDatabaseState) Navigator.pop(context);
+        },
+        builder: (BuildContext context, AppStates state) {
+          cubit = AppCubit.get(context);
+          return Scaffold(
+            key: scaffoldKey,
+            appBar: AppBar(
+              elevation: 0.0,
+              actions: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    right: 10.0,
+                  ),
+                  child: IconButton(
+                    tooltip: !cubit.isLight ? 'Light Mode' : 'Dark Mode',
+                    icon: Icon(
+                      cubit.modeIcon,
+                      color: !cubit.isLight
+                          ? Colors.white
+                          : Colors.black,
+                    ),
+                    onPressed: () {
+                      cubit.toggleMode(
+                        light: !cubit.isLight,
+                        icon:
+                            cubit.isLight ? Icons.light_mode : Icons.dark_mode,
+                        appPrimaryColor: cubit.isLight
+                            ? Colors.black
+                            : Colors.white,
+                        appSecondaryColor: cubit.isLight
+                            ? Colors.white
+                            : Colors.black,
+                      );
+                    },
+                  ),
+                ),
+              ],
+              backgroundColor: cubit.primaryColor,
+              foregroundColor: cubit.secondaryColor,
+              title: Text(
+                cubit.titles[cubit.currentIndex],
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            body: Container(
+              child: cubit.screens[cubit.currentIndex],
+            ),
+            backgroundColor: cubit.primaryColor,
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                if (cubit.isBottomSheetShown) {
+                  if (formKey.currentState!.validate()) {
+                    cubit.insertToDB(
+                      title: titleController.text,
+                      time: timeController.text,
+                      date: dateController.text,
+                    );
+                  }
+                } else {
+                  scaffoldKey.currentState
+                      ?.showBottomSheet(
+                        (context) => Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Form(
+                            key: formKey,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Container(
+                                  height: 5.0,
+                                  width: 100.0,
+                                  decoration: const BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(5.0)),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10.0,
+                                ),
+                                defaultTextFormField(
+                                  controller: titleController,
+                                  type: TextInputType.text,
+                                  validator: (String? value) {
+                                    if (value!.isEmpty) {
+                                      return 'Title must not be empty';
+                                    }
+                                    return null;
+                                  },
+                                  label: 'Task Title',
+                                  prefixIcon: Icons.title,
+                                ),
+                                const SizedBox(
+                                  height: 15.0,
+                                ),
+                                defaultTextFormField(
+                                  controller: timeController,
+                                  readOnlyField: true,
+                                  onTap: () {
+                                    showTimePicker(
+                                      context: context,
+                                      initialTime: TimeOfDay.now(),
+                                    ).then((value) {
+                                      timeController.text =
+                                          value!.format(context).toString();
+                                    });
+                                  },
+                                  type: TextInputType.datetime,
+                                  validator: (String? value) {
+                                    if (value!.isEmpty) {
+                                      return 'Time must not be empty';
+                                    }
+                                    return null;
+                                  },
+                                  label: 'Task Time',
+                                  prefixIcon: Icons.watch_later_outlined,
+                                ),
+                                const SizedBox(
+                                  height: 15.0,
+                                ),
+                                defaultTextFormField(
+                                  controller: dateController,
+                                  readOnlyField: true,
+                                  onTap: () {
+                                    showDatePicker(
+                                      context: context,
+                                      initialDate: DateTime.now(),
+                                      firstDate: DateTime.now(),
+                                      lastDate: DateTime.parse('2030-01-01'),
+                                    ).then((value) {
+                                      dateController.text =
+                                          DateFormat.yMMMd().format(value!);
+                                    });
+                                  },
+                                  type: TextInputType.datetime,
+                                  validator: (String? value) {
+                                    if (value!.isEmpty) {
+                                      return 'Date must not be empty';
+                                    }
+                                    return null;
+                                  },
+                                  label: 'Task Date',
+                                  prefixIcon: Icons.date_range,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        backgroundColor: Colors.white,
+                        elevation: 15.0,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(
+                              25.0,
+                            ),
+                          ),
+                        ),
+                      )
+                      .closed
+                      .then((value) {
+                    cubit.changeBottomSheetState(
+                      isShown: false,
+                      icon: Icons.add_task,
+                    );
+                    titleController.clear();
+                    timeController.clear();
+                    dateController.clear();
+                  });
+                  cubit.changeBottomSheetState(
+                    isShown: true,
+                    icon: Icons.add,
+                  );
+                }
+              },
+              backgroundColor: const Color(0x44696c73),
+              foregroundColor: cubit.secondaryColor,
+              elevation: 0.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  15.0,
+                ),
+              ),
+              child: Icon(
+                cubit.fabIcon,
+              ),
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              currentIndex: AppCubit.get(context).currentIndex,
+              onTap: (index) {
+                AppCubit.get(context).changeIndex(index);
+              },
+              elevation: 10.0,
+              unselectedItemColor: const Color(0xFF696c73),
+              selectedItemColor: cubit.secondaryColor,
+              backgroundColor: cubit.primaryColor,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.task_outlined,
+                  ),
+                  label: 'Tasks',
+                  activeIcon: Icon(
+                    Icons.task,
+                  ),
+                  tooltip: 'Active Tasks',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.check_circle_outline,
+                  ),
+                  label: 'Done',
+                  activeIcon: Icon(
+                    Icons.check_circle,
+                  ),
+                  tooltip: 'Done Tasks',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.archive_outlined,
+                  ),
+                  label: 'Archived',
+                  activeIcon: Icon(
+                    Icons.archive,
+                  ),
+                  tooltip: 'Archived Tasks',
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/shared/components/components.dart
@@ -0,0 +1,265 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/shared/cubit/cubit.dart';
+import 'package:todoapp/shared/cubit/states.dart';
+
+import 'constants.dart';
+
+Widget defaultTextFormField({
+  required TextEditingController controller,
+  required TextInputType type,
+  required String? Function(String?) validator,
+  required String label,
+  required IconData prefixIcon,
+  bool readOnlyField = false,
+  VoidCallback? onTap,
+  Function(String)? onFieldSubmitted,
+  Function(String)? onChanged,
+}) =>
+    TextFormField(
+      controller: controller,
+      keyboardType: type,
+      onFieldSubmitted: onFieldSubmitted,
+      readOnly: readOnlyField,
+      showCursor: true,
+      onChanged: onChanged,
+      validator: validator,
+      onTap: onTap,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(
+          prefixIcon,
+        ),
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(
+              10.0,
+            ),
+          ),
+        ),
+      ),
+    );
+
+Widget taskItem(Map model, context, Color color) => Dismissible(
+      key: Key(model['id'].toString()),
+      background: Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+              ),
+              alignment: AlignmentDirectional.centerStart,
+              color: Colors.red,
+              child: const Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+              ),
+              alignment: AlignmentDirectional.centerEnd,
+              color: Colors.red,
+              child: const Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${model['title']}',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: color,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5.0,
+                  ),
+                  Text(
+                    '${model['date']} ・ ${model['time']}',
+                    style: const TextStyle(
+                      color: Color(0x998D8D8D),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              width: 20.0,
+            ),
+            addTaskControls(model, context),
+          ],
+        ),
+      ),
+      onDismissed: (direction) {},
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (newContext) {
+            return AlertDialog(
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    20.0,
+                  ),
+                ),
+              ),
+              title: const Text('Delete Task?'),
+              content: const Text('Task will be deleted permanently'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    AppCubit.get(context).deleteDB(
+                      id: model['id'],
+                    );
+                    return Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'DELETE',
+                    style: TextStyle(
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: const Text('CANCEL'),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+
+Widget addTaskControls(Map model, context) {
+  if (model['status'] == 'archive') {
+    return IconButton(
+      onPressed: () {
+        AppCubit.get(context).updateDB(
+          status: 'done',
+          id: model['id'],
+        );
+      },
+      icon: doneIcon(cubit.secondaryColor),
+      tooltip: 'Mark as done',
+    );
+  } else if (model['status'] == 'done') {
+    return IconButton(
+      onPressed: () {
+        AppCubit.get(context).updateDB(
+          status: 'archive',
+          id: model['id'],
+        );
+      },
+      icon: archiveIcon(cubit.secondaryColor),
+      tooltip: 'Archive task',
+    );
+  } else {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () {
+            AppCubit.get(context).updateDB(
+              status: 'done',
+              id: model['id'],
+            );
+          },
+          icon: doneIcon(cubit.secondaryColor),
+          tooltip: 'Mark as done',
+        ),
+        IconButton(
+          onPressed: () {
+            AppCubit.get(context).updateDB(
+              status: 'archive',
+              id: model['id'],
+            );
+          },
+          icon: archiveIcon(cubit.secondaryColor),
+          tooltip: 'Archive task',
+        ),
+      ],
+    );
+  }
+}
+
+Icon doneIcon(Color color) => Icon(
+      Icons.check_circle,
+      color: color,
+    );
+
+Icon archiveIcon(Color color) => Icon(
+      Icons.archive,
+      color: color,
+    );
+
+Widget tasksBuilder({
+  required List<Map> tasks,
+  required AppStates state,
+}) =>
+    tasks.isNotEmpty
+        ? ListView.separated(
+            itemBuilder: (context, index) => taskItem(
+              tasks[index],
+              context,
+              cubit.secondaryColor,
+            ),
+            separatorBuilder: (context, index) => Container(
+              margin: const EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+              ),
+              height: 1.0,
+              color: cubit.secondaryColor,
+            ),
+            itemCount: tasks.length,
+            physics: const BouncingScrollPhysics(),
+          )
+        : (state is AppGetDatabaseState ||
+                state is AppChangeBottomNavBarState ||
+                state is AppChangeBottomSheetState ||
+                state is AppChangeAppMode)
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.task,
+                      size: 100.0,
+                      color: cubit.secondaryColor,
+                    ),
+                    Text(
+                      'No Tasks',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: cubit.secondaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            : Center(
+                child: CircularProgressIndicator(
+                  color: cubit.secondaryColor,
+                ),
+              );
